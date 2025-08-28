d6361e51e94a3ce2fd3686355a8ac0fb12fbce06
jmhun <jmhun@JudahPC.unl.edu>
2025-08-28 16:14:55 -0500

    Added Judah Name

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,16 @@
-
+/**
+ * Author: Judah Huntoon Trey Siske
+ * Date: 2025/08/28
+ * A simple program that prints info
+ * @author Judah-huntoon
+ * 
+ */
 public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Computer Science");
+		System.out.println("Judah Huntoon");
+		System.out.println("Also Computer Science");
 	}
 
 }
